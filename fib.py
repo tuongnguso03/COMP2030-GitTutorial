69ed0498d3b13ee0f77c475ef5ccf59cec11181e
tuongnguso03 <metalwallcrusher@gmail.com>
2023-11-17 15:28:53 +0700

    Fix zeroth test by adding zeroth case

--- fib.py
+++ fib.py
@@ -7,5 +7,7 @@ Negative numbers should return None
 def fibonacci(position):
   if(position == 1 or position == 2):
     return 1
+  elif position == 0:
+    return 0
   return fibonacci(position - 1) + fibonacci(position - 2)
   
